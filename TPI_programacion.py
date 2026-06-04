21e2fea77eb876735e46a966a1f7683b0160a306
agustinablobo-cloud <agustinablobo@gmail.com>
2026-06-04 16:25:33 -0300

    agrege la funcion de agregar_pais

--- TPI_programacion.py
+++ TPI_programacion.py
@@ -12,25 +12,26 @@ def cargar_paises(nombre_archivo):
     paises = []
 
     try:
-        with open(nombre_archivo, "r") as archivo:
-         lector = csv.DictReader(archivo)
+        with open(nombre_archivo, "r", encoding="utf-8") as archivo:
+            lector = csv.DictReader(archivo, delimiter=";")
         
 
-        for fila in lector:
+            for fila in lector:
             
-            pais = {
-                "nombre":     fila["nombre"],
-                "poblacion":  int(fila["poblacion"]),
-                "superficie": int(fila["superficie"]),
-                "continente": fila["continente"]
-            }
-            paises.append(pais)     
+                pais = {
+                    "nombre":     fila["nombre"],
+                    "poblacion":  int(fila["poblacion"]),
+                    "superficie": int(fila["superficie"]),
+                    "continente": fila["continente"]
+                }
+                
+                paises.append(pais)     
 
     except FileNotFoundError:
         print("Error: No se encontro el archivo")
 
-    except ValueError:
-       print("Error: Formato numerico incorrecto en el CSV")
+    except ValueError as e:
+       print(f"ValueError: {e} ")
 
     except KeyError:
        print("Error: Faltan columnas en el CSV")
@@ -39,6 +40,11 @@ def cargar_paises(nombre_archivo):
         print("Ocurrió el siguiente error: ", type(e).__name__ )
 
     return paises
+
+
+# --------------------------------------------
+# FUNCION 2: Guardar paises en el CSV
+# --------------------------------------------
 
 def guardar_paises(nombre_archivo, paises):
 
@@ -56,8 +62,95 @@ def guardar_paises(nombre_archivo, paises):
              pais["continente"]
           ])
 
-def agregar_pais():
-    pass
+def agregar_pais(paises):
+    
+    while True:
+       try:
+          
+            nombre = input("Ingrese el nombre del pais: ").strip()
+
+            if not nombre:
+                raise ValueError("Error: el nombre no puede estar vacio")
+        
+            for pais in paises:
+             if pais["nombre"].lower() == nombre.lower():
+                raise ValueError("Error: el pais ya existe")
+           
+            break
+       
+       except ValueError as e:
+          print(e)
+
+    while True:
+       try:
+          poblacion = input("Ingresa la poblacion: ")
+
+          if poblacion == "":
+             raise ValueError("Error: no puede estar vacio")
+          
+          if not poblacion.isdigit():
+             raise ValueError("Error: debe ingresar un numero entero")
+          
+          poblacion_a_cargar = int(poblacion)
+
+          if poblacion_a_cargar <= 0:
+             raise ValueError("Error: la poblacion debe ser mayor a 0")
+          
+          break
+       
+       except ValueError as e:
+          print(e)
+
+
+    while True:
+       try:
+          superficie = input("Ingresa la superficie en km²: " )
+
+          if superficie == "":
+             raise ValueError("Error: no puede estar vacio")
+          
+          if not superficie.isdigit():
+             raise ValueError("Error: debe ingresar un numero entero")
+          
+          superficie_a_cargar = int(superficie)
+
+          if superficie_a_cargar <= 0:
+             raise ValueError("Error: la superficie debe ser mayor a 0")
+          
+          break
+       
+       except ValueError as e:
+          print(e)
+       
+
+    while True:
+       try:
+            continente = input("Ingresa el continente: ").strip()
+
+            if not continente:
+                raise ValueError("Error: el continente no puede estar vacio")
+        
+            
+            break
+       
+       except ValueError as e:
+          print(e)
+
+    nuevo_pais = {
+        "nombre": nombre,
+        "poblacion": poblacion_a_cargar,
+        "superficie": superficie_a_cargar,
+        "continente": continente
+    }
+
+    paises.append(nuevo_pais)
+
+    guardar_paises("paises.csv", paises)
+
+    print("Pais agregado correctamente")
+          
+
+
 
 def actualizar_pais():
    pass
@@ -88,5 +181,6 @@ def mostrar_estadisticas():
 
 
 paises = cargar_paises("paises.csv")
+
 
 #Deberia estar el menu 
